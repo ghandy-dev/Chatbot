e4f24bc7d3239740e886c5a6f33283b03aa8e990
ghandy-dev <85082684+ghandy-dev@users.noreply.github.com>
2024-07-26 19:27:57 +0100

    Remove uneeded middleware rewrites

--- src/Chatbot.Fornax/Serve.fs
+++ src/Chatbot.Fornax/Serve.fs
@@ -1,7 +1,6 @@
 module Serve
 
 open Microsoft.AspNetCore.Builder
-open Microsoft.AspNetCore.Rewrite
 
 let serve () =
     let builder =
@@ -9,12 +8,7 @@ let serve () =
 
     let app = builder.Build()
 
-    let rewriteOptions = new RewriteOptions()
-    rewriteOptions.AddRewrite(@"/", replacement = "index.html", skipRemainingRules = true) |> ignore
-    rewriteOptions.AddRewrite(@"^(.*[^/])$", replacement = "$1.html", skipRemainingRules = true) |> ignore
-
     app.UseDefaultFiles() |> ignore
-    app.UseRewriter(rewriteOptions) |> ignore
     app.UseStaticFiles(new StaticFileOptions()) |> ignore
     app.UseHttpsRedirection() |> ignore
 
