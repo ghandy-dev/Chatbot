083ce006b21ac8eb18dcc4ffd853ba8c207dc351
ghandy-dev <85082684+ghandy-dev@users.noreply.github.com>
2024-08-29 17:05:06 +0100

    Replace newline separators separetely

--- src/Chatbot.Commands/UrbanDictionary/UrbanDictionary.fs
+++ src/Chatbot.Commands/UrbanDictionary/UrbanDictionary.fs
@@ -8,13 +8,11 @@ module Reddit =
 
     open System.Text.RegularExpressions
 
-    let pattern = @"[\[\]]|\n"
-
     let urban args =
         async {
             let getTerm =
                 match args with
-                | [] -> random()
+                | [] -> random ()
                 | _ ->
                     let query = args |> String.concat " "
                     search query
@@ -24,8 +22,10 @@ module Reddit =
             | Ok terms ->
                 match terms with
                 | [] -> return Message "No definition found"
-                | t :: _ ->
-                    let definition = Regex.Replace(t.Definition, pattern, "")
-                    return Message $"{t.Permalink} (+{t.ThumbsUp}/-{t.ThumbsDown}) {t.Word}: {definition}"
+                | term :: _ ->
+                    let definition =
+                        [ @"[\[\]]", "" ; @"(\r\n|\n)", " " ]
+                        |> List.fold (fun acc (pattern, replacement) -> Regex.Replace(acc, pattern, replacement)) term.Definition
 
+                    return Message $"{term.Permalink} (+{term.ThumbsUp}/-{term.ThumbsDown}) {term.Word}: {definition}"
         }
