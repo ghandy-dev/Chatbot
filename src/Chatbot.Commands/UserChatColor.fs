d2a6bbd8f1e968661b048a63de346b4fc0704a1e
ghandy-dev <85082684+ghandy-dev@users.noreply.github.com>
2024-09-20 15:12:25 +0100

    Fix to use UserId instead of Username

--- src/Chatbot.Commands/UserChatColor.fs
+++ src/Chatbot.Commands/UserChatColor.fs
@@ -10,7 +10,11 @@ module NameColor =
             let username =
                 args |> List.tryHead |> Option.bind Some |?? context.Username
 
-            match! Chat.getUserChatColor username |-> Result.fromOption "User not found" with
+            match!
+                Users.getUser username
+                |-> Result.fromOption "User not found"
+                |> Result.bindAsync (fun user -> Chat.getUserChatColor user.Id |-> Result.fromOption "User not found")
+            with
             | Error err -> return Message err
             | Ok response -> return Message $"{response.UserName} {response.Color}"
         }
