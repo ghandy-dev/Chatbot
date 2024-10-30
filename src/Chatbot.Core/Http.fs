45286bdf503c5bd4edf91f7fb58c8eca2cddb7b9
ghandy-dev <85082684+ghandy-dev@users.noreply.github.com>
2024-10-30 16:06:49 +0000

    Add additional http function, include user agent in http requests

--- src/Chatbot.Core/Http.fs
+++ src/Chatbot.Core/Http.fs
@@ -1,32 +1,56 @@
 module Http
 
+open Configuration
+
 open FsHttp
 open FsHttp.Request
 open FsHttp.Response
 
-let getFromJsonAsync<'a> (url: string) =
+let userAgent = configuration.Item("UserAgent")
+
+let getFromJsonAsync<'T> (url: string) =
     async {
         use! response =
             http {
                 GET url
                 Accept MimeTypes.applicationJson
+                UserAgent userAgent
             }
             |> sendAsync
 
         match toResult response with
         | Ok response ->
-            let! deserialized = response |> deserializeJsonAsync<'a>
+            let! deserialized = response |> deserializeJsonAsync<'T>
             return Ok deserialized
         | Error err ->
             let! content = response.content.ReadAsStringAsync() |> Async.AwaitTask
             return Error(content, err.statusCode)
     }
 
-let postAsync (url: string) (data: (string * string) list) =
+let postAsJsonAsync<'T> (url: string, value: 'T) =
     async {
         use! response =
             http {
                 POST url
+                UserAgent userAgent
+                body
+                jsonSerialize value
+            }
+            |> sendAsync
+
+        let! content = response.content.ReadAsStringAsync() |> Async.AwaitTask
+
+        match toResult response with
+        | Ok _ -> return Ok content
+        | Error err -> return Error(content, err.statusCode)
+    }
+
+let postAsync (url: string, data: (string * string) list) =
+    async {
+        use! response =
+            http {
+                POST url
+                UserAgent userAgent
                 body
                 formUrlEncoded data
             }
