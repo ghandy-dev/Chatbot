2aff2836ad90e2864123f47721f50d7a14e08698
ghandy-dev <85082684+ghandy-dev@users.noreply.github.com>
2024-10-30 16:05:58 +0000

    Remove redundant function

--- src/Chatbot.Commands/UrbanDictionary/Api.fs
+++ src/Chatbot.Commands/UrbanDictionary/Api.fs
@@ -3,47 +3,26 @@ namespace Commands.UrbanDictionary
 module Api =
 
     open Types
-    open Configuration
-
-    open FsHttp
-    open FsHttp.Request
-    open FsHttp.Response
-
 
     let [<Literal>] private ApiUrl = "https://api.urbandictionary.com/v0"
 
     let private randomUrl = $"{ApiUrl}/random"
     let private searchUrl term = $"{ApiUrl}/define?term={term}"
 
-    let userAgent = configuration.Item("UserAgent")
-
-    let getFromJsonAsync<'a> url =
-        async {
-            use! response =
-                http {
-                    GET url
-                    Accept MimeTypes.applicationJson
-                    UserAgent userAgent
-                }
-                |> sendAsync
-
-            match toResult response with
-            | Ok response ->
-                let! deserialized = response |> deserializeJsonAsync<'a>
-                return Ok deserialized
-            | Error err -> return Error $"UrbanDictionary API HTTP error {err.statusCode |> int} {err.statusCode}"
-        }
-
     let random () =
         async {
-            match! getFromJsonAsync<Terms>randomUrl with
-            | Error err -> return Error err
+            match! Http.getFromJsonAsync<Terms>randomUrl with
+            | Error (content, statusCode) ->
+                Logging.error $"Urban Dictionary API error: {content}" (new System.Net.Http.HttpRequestException("Urban Dictionary API error", null, statusCode))
+                return Error "Urban Dictionary API error"
             | Ok definitions -> return Ok definitions.list
         }
 
     let search term =
         async {
-            match! getFromJsonAsync<Terms>(searchUrl term) with
-            | Error err -> return Error err
+            match! Http.getFromJsonAsync<Terms>(searchUrl term) with
+            | Error (content, statusCode) ->
+                Logging.error $"Urban Dictionary API error: {content}" (new System.Net.Http.HttpRequestException("Urban Dictionary API error", null, statusCode))
+                return Error "Urban Dictionary API error"
             | Ok definitions -> return Ok definitions.list
         }
