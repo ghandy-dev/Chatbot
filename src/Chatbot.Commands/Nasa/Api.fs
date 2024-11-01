dc03413b0ae449782c5f49eedd2cd1446f79aedb
ghandy-dev <85082684+ghandy-dev@users.noreply.github.com>
2024-11-01 15:14:55 +0000

    Refactor of Nasa API to use helper http methods, and small refactor of `apod` command

--- src/Chatbot.Commands/Nasa/Api.fs
+++ src/Chatbot.Commands/Nasa/Api.fs
@@ -5,10 +5,6 @@ module Nasa =
     open Commands.Types.Nasa
     open Configuration
 
-    open FsHttp
-    open FsHttp.Request
-    open FsHttp.Response
-
     open System
 
     let private apiKey = Configuration.Nasa.config.ApiKey
@@ -16,40 +12,45 @@ module Nasa =
 
     let [<Literal>] private ApiUrl = "https://api.nasa.gov"
 
-    let private pictureOfTheDay date = $"{ApiUrl}/planetary/apod?api_key={apiKey}&date={date}"
-    let private marsRoverPhotos date camera = $"{ApiUrl}/mars-photos?api_key={apiKey}&date={date}&camera={camera}"
+    let private currentPictureOfTheDay = $"{ApiUrl}/planetary/apod?api_key={apiKey}"
+
+    let private pictureOfTheDay date =
+        $"{ApiUrl}/planetary/apod?api_key={apiKey}&date={date}"
+
+    let private marsRoverPhotos date camera =
+        $"{ApiUrl}/mars-photos?api_key={apiKey}&date={date}&camera={camera}"
 
     let private dateFormat = "yyyy-MM-dd"
 
-    let private getFromJsonAsync<'a> url =
+    let getCurrentPictureOfTheDay () =
         async {
-            use! response =
-                http {
-                    GET url
-                    Accept MimeTypes.applicationJson
-                    UserAgent userAgent
-                }
-                |> sendAsync
+            let url = currentPictureOfTheDay
 
-            match toResult response with
-            | Ok response ->
-                let! deserialized = response |> deserializeJsonAsync<'a>
-                return Ok deserialized
-            | Error err -> return Error $"NASA API HTTP error {err.statusCode |> int} {err.statusCode}"
+            match! Http.getFromJsonAsync<APOD> url with
+            | Error(err, statusCode) ->
+                Logging.error $"NASA API error: {err}" (new System.Net.Http.HttpRequestException("", null, statusCode))
+                return None
+            | Ok apod -> return Some apod
         }
 
-    let getPictureOfTheDay (date: DateOnly option) =
+    let getPictureOfTheDay (date: DateOnly) =
         async {
-            let date = date |?? DateOnly.FromDateTime(DateTimeOffset.UtcNow.DateTime)
             let url = pictureOfTheDay (date.ToString(dateFormat))
 
-            return! getFromJsonAsync<APOD> url
+            match! Http.getFromJsonAsync<APOD> url with
+            | Error(err, statusCode) ->
+                Logging.error $"NASA API error: {err}" (new System.Net.Http.HttpRequestException("", null, statusCode))
+                return None
+            | Ok apod -> return Some apod
         }
 
-    let getMarsRoverPhoto (camera: RoverCamera) (date: DateOnly option) =
+    let getMarsRoverPhoto (camera: RoverCamera) (date: DateOnly) =
         async {
-            let date = date |?? DateOnly.FromDateTime(DateTimeOffset.UtcNow.DateTime)
             let url = marsRoverPhotos date camera
-            return! getFromJsonAsync<APOD> url
-        }
 
+            match! Http.getFromJsonAsync<APOD> url with
+            | Error(err, statusCode) ->
+                Logging.error $"NASA API error: {err}" (new System.Net.Http.HttpRequestException("", null, statusCode))
+                return None
+            | Ok apod -> return Some apod
+        }
--- src/Chatbot.Commands/Nasa/Nasa.fs
+++ src/Chatbot.Commands/Nasa/Nasa.fs
@@ -8,27 +8,32 @@ module Nasa =
     open Commands.Api.Nasa
     open Commands.Types.Nasa
 
-    let private parseApodArgs (args: string list) =
-        match args with
-        | [] -> Ok None
-        | date :: _ ->
-            match DateOnly.TryParse(date) with
-            | false, _ -> Error $"Couldn't parse provided date"
-            | true, parsedDate -> Ok <| Some parsedDate
+    let private parseApodArgs (date: string) =
+        match DateOnly.TryParse(date) with
+        | false, _ -> None
+        | true, parsedDate -> Some parsedDate
+
+    let private formatApodMessage (apod: APOD) =
+        let url =
+            match apod.HdUrl with
+            | None -> apod.Url
+            | Some url -> url
+
+        sprintf "%s %s" apod.Title url
 
     let apod args =
         async {
-            match!
-                parseApodArgs args
-                |> Async.create
-                |> Result.bindAsync getPictureOfTheDay
-            with
-            | Error err -> return Message err
-            | Ok apod ->
-                let url =
-                    match apod.HdUrl with
-                    | None -> apod.Url
-                    | Some url -> url
-
-                return Message $"{apod.Title} {url}"
+            match args with
+            | [] ->
+                match! getCurrentPictureOfTheDay () with
+                | None -> return Message "Couldn't get todays picture"
+                | Some apod -> return Message (formatApodMessage apod)
+            | args ->
+                let maybeDate = parseApodArgs (args |> String.concat " ")
+                match maybeDate with
+                | None -> return Message "Couldn't parse date"
+                | Some date ->
+                    match! getPictureOfTheDay date with
+                    | None -> return Message "Couldn't get todays picture"
+                    | Some apod -> return Message (formatApodMessage apod)
         }
