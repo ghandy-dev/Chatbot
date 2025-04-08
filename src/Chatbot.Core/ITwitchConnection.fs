e9f66b60e4aa3066de84b534e9f8a4869a3dbe7d
ghandy-dev <85082684+ghandy-dev@users.noreply.github.com>
2025-04-08 13:43:26 +0100

    Formatting

--- src/Chatbot.Core/ITwitchConnection.fs
+++ src/Chatbot.Core/ITwitchConnection.fs
@@ -3,8 +3,8 @@ namespace Clients
 [<AllowNullLiteral>]
 type ITwitchConnection =
     inherit System.IDisposable
-    abstract member Connected : bool
-    abstract member ConnectAsync : cancellationToken: System.Threading.CancellationToken -> Async<unit>
-    abstract member ReadAsync : cancellationToken: System.Threading.CancellationToken -> Async<string option>
-    abstract member SendAsync : message: string -> Async<unit>
-    abstract member AuthenticateAsync : user: string * token: string * capabailities: string array -> Async<unit>
+    abstract member Connected: bool
+    abstract member ConnectAsync: cancellationToken: System.Threading.CancellationToken -> Async<unit>
+    abstract member ReadAsync: cancellationToken: System.Threading.CancellationToken -> Async<string option>
+    abstract member SendAsync: message: string -> Async<unit>
+    abstract member AuthenticateAsync: user: string * token: string * capabailities: string array -> Async<unit>
