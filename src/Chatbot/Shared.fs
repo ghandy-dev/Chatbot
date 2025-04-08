fd3d47e52b4d10422fe38372d40bd7839255302a
ghandy-dev <85082684+ghandy-dev@users.noreply.github.com>
2025-04-08 15:57:37 +0100

    Remove attempt at avoiding duplicate message block

--- src/Chatbot/Shared.fs
+++ src/Chatbot/Shared.fs
@@ -12,24 +12,7 @@ let userStates = new ConcurrentDictionary<string, UserState>()
 let channelStates = new ConcurrentDictionary<string, RoomState>()
 
 let formatChatMessage (message: string) =
-    let message =
-        if message.Length > 500 then
-            message[0..494] + "..."
-        else
-            message
-
-    let maybeIndex =
+    if message.Length > 500 then
+        message[0..496] + "..."
+    else
         message
-        |> Seq.indexed
-        |> Seq.filter (fun (_, c) -> c = ' ')
-        |> Seq.tryRandomChoice
-
-    match maybeIndex with
-    | None -> message
-    | Some (index, _) ->
-        let sb = new StringBuilder()
-        let span = message.AsSpan()
-        sb.Append(span.Slice(0, index)) |> ignore
-        sb.Append(" \U000e0000") |> ignore
-        sb.Append(span.Slice(index, message.Length - index)) |> ignore
-        sb.ToString()
