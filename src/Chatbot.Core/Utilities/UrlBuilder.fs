0c97b5ae7fe9941180d315326b39aa33ada9074e
ghandy-dev <85082684+ghandy-dev@users.noreply.github.com>
2026-03-05 19:01:29 +0000

    Added url builder helper function to sanitise url query parameters

---
+++ src/Chatbot.Core/Utilities/UrlBuilder.fs
@@ -0,0 +1,11 @@
+module UrlBuilder
+
+open System
+
+let buildUrl url (parameters: (string * string) seq) =
+    let query =
+        parameters
+        |> Seq.map (fun (k, v) -> $"{k}={Uri.EscapeDataString(v)}")
+        |> strJoin "&"
+
+    $"{url}?{query}"
