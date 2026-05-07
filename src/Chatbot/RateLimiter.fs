20a9600df35d6b12a757d85a8dde7bc1b6d6f4df
ghandy-dev <85082684+ghandy-dev@users.noreply.github.com>
2026-05-07 13:57:27 +0100

    Rename variables

--- src/Chatbot/RateLimiter.fs
+++ src/Chatbot/RateLimiter.fs
@@ -11,7 +11,7 @@ let [<Literal>] GlobalSlow = 1000
 let [<Literal>] MessageLimit_Chat = 20 // per 30 seconds
 let [<Literal>] ResetInterval_Chat = 30s
 
-let [<Literal>] MessageLimit_Whispers = 100
+let [<Literal>] MessageLimit_Whispers = 100 // per 60 seconds
 let [<Literal>] ResetInterval_Whispers = 60s
 
 type RateLimitState = {
@@ -30,20 +30,20 @@ module RateLimitState =
 
 type RateLimiter(messageLimit, interval: int16) =
 
-    let channels = new Dictionary<string, RateLimitState>()
+    let cache = new Dictionary<string, RateLimitState>()
 
-    member _.CanSend channel =
+    member _.CanSend key =
         let now = epochTime ()
 
-        match channels |> Dict.tryGetValue channel with
+        match cache |> Dict.tryGetValue key with
         | Some rates when now > rates.ResetAt && now - rates.LastMessageTimestamp > int64 GlobalSlow + 200L ->
-            channels[channel] <- RateLimitState.create (epochTime() + int64 interval)
+            cache[key] <- RateLimitState.create (epochTime() + int64 interval)
             true
         | Some rates when rates.MessageCount < messageLimit && now - rates.LastMessageTimestamp > int64 GlobalSlow + 200L ->
-            channels[channel] <- { rates with MessageCount = rates.MessageCount + 1 }
+            cache[key] <- { rates with MessageCount = rates.MessageCount + 1 }
             true
         | Some rates ->
             false
         | None ->
-            channels[channel] <- RateLimitState.create (epochTime() + int64 interval)
+            cache[key] <- RateLimitState.create (epochTime() + int64 interval)
             true
