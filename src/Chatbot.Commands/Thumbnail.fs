0c71c044f43fd7fba04fb421b4ffa6c342ce1649
ghandy-dev <85082684+ghandy-dev@users.noreply.github.com>
2026-05-10 20:55:34 +0100

    Upload an image of the current thumbnail (the live thumbnail automatically updates at set intervals)

--- src/Chatbot.Commands/Thumbnail.fs
+++ src/Chatbot.Commands/Thumbnail.fs
@@ -5,11 +5,16 @@ module StreamThumbnail =
 
     open FsToolkit.ErrorHandling
 
+    open Chatbot.Core
     open Chatbot.Core.Domain.Commands
     open Chatbot.Core.Domain.Commands.CommandError
     open Chatbot.Core.Services.Twitch
+    open Chatbot.Core.Services.ImageUpload
+    open Chatbot.Core.Types
 
-    let thumbnail (twitchService: TwitchService) context =
+    let thumbnail env (twitchService: TwitchService) (imageUploadService: IImageUploadService) context =
+        let httpClient = env.HttpClient
+
         asyncResult {
             let! channel =
                 match context.MessageArgs with
@@ -19,6 +24,18 @@ module StreamThumbnail =
             match! twitchService.Streams.GetStream channel |> AsyncResult.mapError (CommandHttpError.fromHttpStatusCode "Twitch") with
             | None -> return [ Message "Channel not found or is not currently live" ]
             | Some stream ->
-                let url = stream.ThumbnailUrl.Replace("{width}", "640").Replace("{height}", "360")
+                let thumbnail = stream.ThumbnailUrl.Replace("{width}", "640").Replace("{height}", "360")
+
+                let request = Http.Request.get thumbnail
+                let! response = Http.send httpClient request
+
+                let! bytes =
+                    response
+                    |> Http.Response.toResult
+                    |> Result.eitherMap _.Bytes _.StatusCode
+                    |> Result.mapError (CommandHttpError.fromHttpStatusCode "nuuls")
+
+                let! url = imageUploadService.Upload bytes |> AsyncResult.mapError (CommandHttpError.fromHttpStatusCode "nuuls")
+
                 return [ Message url ]
         }
